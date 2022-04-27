d162f5e7e28f31891a4cfb6c4dfca096022e44af
spelui <vlad.spelui@gmail.com>
2022-04-27 19:47:38 +0300

    add navigate to logOut btn

--- src/components/UserInf/UserInf.jsx
+++ src/components/UserInf/UserInf.jsx
@@ -1,4 +1,6 @@
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+
 import { auth } from "../../firebase";
 import Filter from "../Filter/Filter";
 import { FiLogOut } from "react-icons/fi";
@@ -6,6 +8,12 @@ import "./UserInf.scss";
 
 const UserInf = () => {
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const logOutBtn = () => {
+    auth.signOut();
+    navigate("/");
+  };
   return (
     <div className="userInf">
       <div className="userInf__account">
@@ -21,7 +29,7 @@ const UserInf = () => {
           <p className="userInf__name">{user.displayName}</p>
         </div>
 
-        <button className="userInf__btn" onClick={() => auth.signOut()}>
+        <button className="userInf__btn" onClick={() => logOutBtn()}>
           <FiLogOut />
         </button>
       </div>
